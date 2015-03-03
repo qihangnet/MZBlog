fd350211df74f40f6e0f7e8620414d229c3359df
qihangnet <qihangnet@hotmail.com>
2015-03-03 13:58:57 +0800

    补上忘记commit的文件

---
+++ MZBlog.Core.Tests/iBoxDBBackedTest.cs
@@ -0,0 +1,23 @@
+﻿using iBoxDB.LocalServer;
+using MZBlog.Core.Documents;
+using System;
+
+namespace MZBlog.Core.Tests
+{
+    public class iBoxDBBackedTest : IDisposable
+    {
+        protected readonly DB.AutoBox _db;
+
+        public iBoxDBBackedTest()
+        {
+            var server = new DB();
+            server.GetConfig().EnsureTable<Author>(DBTableNames.Authors, "Id");
+            _db = server.Open();
+        }
+
+        public void Dispose()
+        {
+            _db.GetDatabase().Dispose();
+        }
+    }
+}
---
+++ MZBlog.Core/DBTableNames.cs
@@ -0,0 +1,11 @@
+﻿namespace MZBlog.Core
+{
+    public class DBTableNames
+    {
+        public const string Authors = "Authors";
+        public const string BlogComments = "BlogComments";
+        public const string BlogPosts = "BlogPosts";
+        public const string SpamHashes = "SpamHashes";
+        public const string Tags = "Tags";
+    }
+}
