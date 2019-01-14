87bdbc9cf92fe72dcc275ba949dbb9bc86d129f0
qihangnet <mzyi@soez.io>
2019-01-14 17:50:37 +0800

    修正文章正文在内容很窄的情况下的布局问题

--- MZBlog.Web/Pages/Index.cshtml.cs
+++ MZBlog.Web/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using System.Net.Sockets;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MZBlog.Core.ViewProjections.Home;
@@ -8,14 +9,15 @@ namespace MZBlog.Web.Pages
     public class IndexModel : PageModel
     {
         private readonly IMediator _mediator;
+        private readonly IPIP.Net.City _ipCity;
 
-        public IndexModel(IMediator mediator)
+        public IndexModel(IMediator mediator, IPIP.Net.City ipCity)
         {
             _mediator = mediator;
+            _ipCity = ipCity;
         }
 
         public RecentBlogPostsViewModel RecentBlogPosts { get; set; }
-
 
         public void OnGet()
         {
@@ -26,6 +28,15 @@ namespace MZBlog.Web.Pages
             }
             var query = new RecentBlogPostsQuery() { Page = pageNo, Take = 10 };
             RecentBlogPosts = _mediator.Send(query).Result;
+            // var ipAddress = Request.HttpContext.Connection.RemoteIpAddress;
+            // if (ipAddress.AddressFamily == AddressFamily.InterNetwork)
+            // {
+            //     var ss = _ipCity.Find(ipAddress.ToString());//"115.159.114.205");
+            //     if (ss.Length > 0)
+            //         System.Console.WriteLine(string.Join(",", ss));
+            //     else
+            //         System.Console.WriteLine("无数据");
+            // }
         }
     }
 }
